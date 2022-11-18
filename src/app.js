b9762db8318847fbcc151a8d7f765265c7e99393
Khoironi Kurnia Syah <zekhoi.app@gmail.com>
2022-11-18 18:09:20 +0800

    sdads

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    message: "Hello World",
+  });
 });
 
 module.exports = app;
